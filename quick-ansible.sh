8607667a3fe6352ebb0f57309912f03c0a1b33c2
Ismail Marmoush <marmoushismail@gmail.com>
2018-09-21 17:43:23 +0200

    A Quick Commit!

--- quick-ansible.sh
+++ quick-ansible.sh
@@ -4,15 +4,20 @@ IMAGE_NAME=ismailmarmoush/ansible-docker
 
 # Change here to fit your needs
 HOST_SSH=$HOME/.ssh
-HOST_GIT=$HOME/git
+host_workdir=.
 
 # Change here if you know what you're doing
 MACHINE_SSH=/root/.ssh
-MACHINE_GIT=/root/git
+machine_workdir=/root/git
 
 ansible_run(){
 	code="${@:2}"
-	docker run -it -e CODE="$1 ${code}" -v $HOST_SSH:$MACHINE_SSH -v $HOST_GIT:$MACHINE_GIT $IMAGE_NAME
+	docker run -it \
+	    -e CODE="$1 ${code}" \
+	    -v $HOST_SSH:$MACHINE_SSH \
+	    -v $host_workdir:$machine_workdir \
+	    -w $machine_workdir \
+	    $IMAGE_NAME
 }
 
 ansible(){
