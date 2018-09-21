c328016152d45f5b62693f8377480bb3c90c496f
Ismail Marmoush <marmoushismail@gmail.com>
2018-09-21 17:12:33 +0200

    A Quick Commit!

--- quick-ansible.sh
+++ quick-ansible.sh
@@ -10,18 +10,57 @@ HOST_GIT=$HOME/git
 MACHINE_SSH=/root/.ssh
 MACHINE_GIT=/root/git
 
+ansible_run(){
+	code="${@:2}"
+	docker run -it -e CODE="$1 ${code}" -v $HOST_SSH:$MACHINE_SSH -v $HOST_GIT:$MACHINE_GIT $IMAGE_NAME
+}
+
 ansible(){
-	CODE="$@"
-	docker run -it -e CODE="ansible ${CODE}" -v $HOST_SSH:$MACHINE_SSH -v $HOST_GIT:$MACHINE_GIT $IMAGE_NAME
+    ansible_run ansible "$@"
 }
 
 ansible_playbook(){
-	CODE="$@"
-	docker run -it -e CODE="ansible-playbook ${CODE}" -v $HOST_SSH:$MACHINE_SSH -v $HOST_GIT:$MACHINE_GIT  $IMAGE_NAME
+    ansible_run ansible-playbook "$@"
+}
+
+ansible_galaxy(){
+    ansible_run ansible_galaxy "$@"
+}
+
+ansible_config(){
+    ansible_run ansible_config "$@"
+}
+
+ansible_connection(){
+    ansible_run ansible_connection "$@"
+}
+
+ansible_console(){
+    ansible_run ansible_console "$@"
+}
+
+ansible_doc(){
+    ansible_run ansible_doc "$@"
+}
+
+ansible_inventory(){
+    ansible_run ansible_inventory "$@"
+}
+
+ansible_pull(){
+    ansible_run ansible_pull "$@"
+}
+
+ansible_test(){
+    ansible_run ansible_test "$@"
+}
+
+ansible_vault(){
+    ansible_run ansible_vault "$@"
 }
 
 ansible_bash(){
-	docker run -it -v $HOST_SSH:$MACHINE_SSH -v $HOST_GIT:$MACHINE_GIT $IMAGE_NAME /bin/bash
+    ansible_run bash "$@"
 }
 
 
