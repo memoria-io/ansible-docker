428f87134c0343b38cdcbc7069f0abbd26f3a1a2
Ismail Marmoush <marmoushismail@gmail.com>
2018-09-21 23:23:21 +0200

    run_ansible

--- quick-ansible.sh
+++ quick-ansible.sh
@@ -5,7 +5,7 @@ IMAGE_NAME=ismailmarmoush/ansible-docker
 HOST_SSH=$HOME/.ssh
 MACHINE_SSH=/root/.ssh
 
-run_ansible(){
+ansible_run(){
 	code="${@:2}"
 	host_workdir=$PWD
     machine_workdir="$PWD"
@@ -22,57 +22,60 @@ run_ansible(){
     # sudo chown -R $(id -u $USER):$(id -g $USER) $PWD
 }
 
-chown_dir(){
-    sudo chown -R $(id -u $USER):$(id -g $USER) $PWD
+ansible-bash(){
+    ansible_run bash "$@"
 }
 
 ansible(){
-    run_ansible ansible "$@"
+    ansible_run ansible "$@"
 }
 
 ansible-playbook(){
-    run_ansible ansible-playbook "$@"
+    ansible_run ansible-playbook "$@"
 }
 
 ansible-galaxy(){
-    run_ansible ansible-galaxy "$@"
+    ansible_run ansible-galaxy "$@"
 }
 
 ansible-config(){
-    run_ansible ansible-config "$@"
+    ansible_run ansible-config "$@"
 }
 
 ansible-connection(){
-    run_ansible ansible-connection "$@"
+    ansible_run ansible-connection "$@"
 }
 
 ansible-console(){
-    run_ansible ansible-console "$@"
+    ansible_run ansible-console "$@"
 }
 
 ansible-doc(){
-    run_ansible ansible-doc "$@"
+    ansible_run ansible-doc "$@"
 }
 
 ansible-inventory(){
-    run_ansible ansible-inventory "$@"
+    ansible_run ansible-inventory "$@"
 }
 
 ansible-pull(){
-    run_ansible ansible-pull "$@"
+    ansible_run ansible-pull "$@"
 }
 
 ansible-test(){
-    run_ansible ansible-test "$@"
+    ansible_run ansible-test "$@"
 }
 
 ansible-vault(){
-    run_ansible ansible-vault "$@"
+    ansible_run ansible-vault "$@"
 }
 
-ansible_bash(){
-    run_ansible bash "$@"
-}
+#######################################################################
+# Utils
+#######################################################################
 
+chown_dir(){
+    sudo chown -R $(id -u $USER):$(id -g $USER) $PWD
+}
 
 "$@"
